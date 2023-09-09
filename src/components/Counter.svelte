f6e52683cafaaeda3c59b87c862d6d76cde9e69a
fentosv <fentos.dev@gmail.com>
2023-09-09 16:55:45 +0200

    Add Counter component with Svelte

---
+++ src/components/Counter.svelte
@@ -0,0 +1,39 @@
+<script>
+  let count = 0;
+</script>
+
+<article>
+  Count: {count}
+  <div>
+    <button on:click={() => count++}>+</button>
+    <button on:click={() => count--}>-</button>
+  </div>
+</article>
+
+<style>
+  article {
+    color: var(--color-title);
+    opacity: 0.8;
+    background-color: var(--background-color);
+    border-radius: var(--border-radius);
+    padding: var(--size-spacing-1);
+    display: flex;
+    padding: var(--size-spacing-2) var(--size-spacing-3);
+    flex-direction: column;
+    gap: var(--size-spacing-1);
+  }
+  button {
+    background: none;
+    border: 1px solid transparent;
+    border-radius: var(--border-radius);
+    font-size: 1.7rem;
+    background-color: #310a65a8;
+    width: 5ch;
+  }
+
+  @media (hover: hover) {
+    button:hover {
+      border-color: var(--color-primary-light-3);
+    }
+  }
+</style>
