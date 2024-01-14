373f455139892dfce6fe002b67391d026e985e4f
fentosv <fentos.dev@gmail.com>
2024-01-14 13:47:42 +0100

    Add eslint-plugin-svelte & rules, align prettierrc & add to local settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,33 @@ module.exports = {
       },
     },
     {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended'],
+      parserOptions: {
+        parser: {
+          // Specify a parser for each lang.
+          ts: '@typescript-eslint/parser',
+          js: 'espree',
+          typescript: '@typescript-eslint/parser',
+        },
+      },
+      // 👉🏽 https://github.com/sveltejs/eslint-plugin-svelte?tab=readme-ov-file#white_check_mark-rules
+      rules: {
+        'svelte/no-target-blank': 'error',
+        'svelte/button-has-type': 'warn',
+        'svelte/html-self-closing': [
+          'error',
+          {
+            void: 'always',
+            normal: 'never',
+            component: 'always',
+            svelte: 'always',
+          },
+        ],
+      },
+    },
+    {
       files: ['*.ts'],
       parser: '@typescript-eslint/parser',
       extends: ['plugin:@typescript-eslint/recommended'],
